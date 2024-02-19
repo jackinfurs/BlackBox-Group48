afe067b09d3b3fa3b646e6fbcb046784c7246e39
Jack D. <jack.dunne4@ucdconnect.ie>
2024-02-19 10:13:58 +0000

    minor changes to config in DesktopLauncher.java

--- desktop/src/com/blackbox/game/DesktopLauncher.java
+++ desktop/src/com/blackbox/game/DesktopLauncher.java
@@ -6,10 +6,10 @@ import com.blackbox.game.BlackBox;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("COMP20050SEP2-48");
-		new Lwjgl3Application(new BlackBox(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 600);
+        config.setTitle("Black Box+");
+        new Lwjgl3Application(new BlackBox(), config);
+    }
 }
