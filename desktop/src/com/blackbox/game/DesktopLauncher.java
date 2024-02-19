43952a650c149e6fd7707750924189e750ed0da0
Jack D. <jack.dunne4@ucdconnect.ie>
2024-02-19 09:49:49 +0000

    set up libgdx framework for use in intellij

---
+++ desktop/src/com/blackbox/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.blackbox.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.blackbox.game.BlackBox;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("COMP20050SEP2-48");
+		new Lwjgl3Application(new BlackBox(), config);
+	}
+}
