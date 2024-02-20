6dfa514c851167976f00ef8bd1ed32b5ebd2272b
Jack D. <jack.dunne4@ucdconnect.ie>
2024-02-20 12:19:26 +0000

    added TODOs, cleaned up code by convention, cleaned up assets dir. must add comments and make Button class.

--- core/src/com/blackbox/game/BlackBox.java
+++ core/src/com/blackbox/game/BlackBox.java
@@ -1,127 +1,25 @@
 package com.blackbox.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class BlackBox extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture backgroundTexture;
-    private Texture playButtonTexture;
-    private Texture leaderboardButtonTexture;
-    private Texture exitButtonTexture;
-    private boolean playButtonClicked;
-    private boolean leaderboardButtonClicked;
-    private boolean exitButtonClicked;
+public class BlackBox extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
 
-    @Override
     public void create() {
         batch = new SpriteBatch();
-
-        // Load menu assets/pngs
-        backgroundTexture = new Texture("VaporBackground.png");
-        playButtonTexture = new Texture("play.png");
-        leaderboardButtonTexture = new Texture("Leaderboard.png");
-        exitButtonTexture = new Texture("Exit.png");
-
-        Gdx.input.setInputProcessor(new InputHandler());
+        font = new BitmapFont(); // use libGDX's default Arial font
+        this.setScreen(new MainMenuScreen(this));
     }
 
-    @Override
     public void render() {
-        // Clear the screen
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Render the main menu
-        batch.begin();
-        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        // Draw play button
-        float buttonWidth = 200;
-        float buttonHeight = 50;
-        float playButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-        float playButtonY = 300;
-        batch.draw(playButtonTexture, playButtonX, playButtonY, buttonWidth, buttonHeight);
-
-        // Draw leaderboard button
-        float leaderboardButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-        float leaderboardButtonY = 400;
-        batch.draw(leaderboardButtonTexture, leaderboardButtonX, leaderboardButtonY, buttonWidth, buttonHeight);
-
-        // Draw exit button
-        float exitButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-        float exitButtonY = 200;
-        batch.draw(exitButtonTexture, exitButtonX, exitButtonY, buttonWidth, buttonHeight);
-
-        batch.end();
-
-        // Process button clicks
-        if (playButtonClicked) {
-            System.out.println("Play button clicked!");
-            playButtonClicked = false; // Reset
-        }
-
-        if (leaderboardButtonClicked) {
-            System.out.println("Leaderboard button clicked!");
-            leaderboardButtonClicked = false; // Reset
-        }
-
-        if (exitButtonClicked) {
-            System.out.println("Exit button clicked!");
-            Gdx.app.exit(); // Exit the application
-        }
+        super.render();
     }
 
-    @Override
     public void dispose() {
         batch.dispose();
-        backgroundTexture.dispose();
-        playButtonTexture.dispose();
-        leaderboardButtonTexture.dispose();
-        exitButtonTexture.dispose();
-    }
-
-    private class InputHandler extends InputAdapter {
-        @Override
-        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-            // Convert screen coordinates to world coordinates
-            float worldX = screenX * Gdx.graphics.getWidth() / (float) Gdx.graphics.getWidth();
-            float worldY = (Gdx.graphics.getHeight() - screenY) * Gdx.graphics.getHeight() / (float) Gdx.graphics.getHeight();
-
-            // Check if the touch is within the bounds of the play button
-            float buttonWidth = 200;
-            float buttonHeight = 50;
-            float playButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-            float playButtonY = 300;
-            if (worldX >= playButtonX && worldX <= playButtonX + buttonWidth && worldY >= playButtonY && worldY <= playButtonY + buttonHeight) {
-                playButtonClicked = true;
-            }
-
-            // Check if the touch is within the bounds of the leaderboard button
-            float leaderboardButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-            float leaderboardButtonY = 400;
-            if (worldX >= leaderboardButtonX &&
-                    worldX <= leaderboardButtonX + buttonWidth &&
-                    worldY >= leaderboardButtonY &&
-                    worldY <= leaderboardButtonY + buttonHeight) {
-                leaderboardButtonClicked = true;
-            }
-
-            // Check if the touch is within the bounds of the exit button
-            float exitButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-            float exitButtonY = 200;
-            if (worldX >= exitButtonX &&
-                    worldX <= exitButtonX + buttonWidth &&
-                    worldY >= exitButtonY &&
-                    worldY <= exitButtonY + buttonHeight) {
-                exitButtonClicked = true;
-            }
-
-            return true;
-        }
+        font.dispose();
     }
 }
---
+++ core/src/com/blackbox/game/MainMenuScreen.java
@@ -0,0 +1,160 @@
+package com.blackbox.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen implements Screen {
+    final BlackBox game;
+    OrthographicCamera camera;
+
+    SpriteBatch batch;
+    Texture backgroundTexture;
+    Texture playButtonTexture;
+    Texture leaderboardButtonTexture;
+    Texture exitButtonTexture;
+    // TODO might switch below for class methods; to make a button class and have .isClicked(), etc
+    //  makes code cleaner when these buttons are objects, shortens code, reduces lines, looks nicer
+    //  all following TODOs have to do with buttons
+    boolean playButtonClicked;
+    boolean exitButtonClicked;
+    boolean leaderboardButtonClicked;
+
+    public MainMenuScreen(final BlackBox game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        // TODO please find a way to set the viewportWidth and Height to something more concrete
+        //  this is exactly the time i'd like a preprocessor directive
+        // TODO also set this to not stretch/borderless, etc
+        camera.setToOrtho(false, 800, 600);
+        batch = new SpriteBatch();
+
+        // Load menu assets/pngs
+        backgroundTexture = new Texture("MainMenuScreen/vaporBackground.png");
+        playButtonTexture = new Texture("MainMenuScreen/play.png");
+        leaderboardButtonTexture = new Texture("MainMenuScreen/leaderboard.png");
+        exitButtonTexture = new Texture("MainMenuScreen/exit.png");
+
+        Gdx.input.setInputProcessor(new InputHandler());
+    }
+
+    float buttonWidth = 200;
+    float buttonHeight = 50;
+
+    private void drawButton(Texture t, int y) {
+        float x = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
+        batch.draw(t, x, y, buttonWidth, buttonHeight);
+    }
+
+    @Override
+    public void render(float v) {
+        // Clear the screen
+        ScreenUtils.clear(0, 0, 0, 1);
+
+        // Render the main menu
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        // TODO
+        // draw buttons
+        drawButton(leaderboardButtonTexture, 400);
+        drawButton(playButtonTexture, 300);
+        drawButton(exitButtonTexture, 200);
+
+        batch.end();
+
+        // Process button clicks
+        if (playButtonClicked) {
+            System.out.println("Play button clicked!");
+            playButtonClicked = false; // Reset
+        }
+
+        if (leaderboardButtonClicked) {
+            System.out.println("Leaderboard button clicked!");
+            leaderboardButtonClicked = false; // Reset
+        }
+
+        if (exitButtonClicked) {
+            System.out.println("Exit button clicked!");
+            Gdx.app.exit(); // Exit the application
+        }
+    }
+
+    private class InputHandler extends InputAdapter {
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            // Convert screen coordinates to world coordinates
+            float worldX = screenX * Gdx.graphics.getWidth() / (float) Gdx.graphics.getWidth();
+            float worldY = (Gdx.graphics.getHeight() - screenY) * Gdx.graphics.getHeight() / (float) Gdx.graphics.getHeight();
+
+            // Check if the touch is within the bounds of the play button
+            float buttonWidth = 200;
+            float buttonHeight = 50;
+            float playButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
+            float playButtonY = 300;
+            if (worldX >= playButtonX && worldX <= playButtonX + buttonWidth && worldY >= playButtonY && worldY <= playButtonY + buttonHeight) {
+                playButtonClicked = true;
+            }
+
+            // TODO
+            // Check if the touch is within the bounds of the leaderboard button
+            float leaderboardButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
+            float leaderboardButtonY = 400;
+            if (worldX >= leaderboardButtonX && worldX <= leaderboardButtonX + buttonWidth && worldY >= leaderboardButtonY && worldY <= leaderboardButtonY + buttonHeight) {
+                leaderboardButtonClicked = true;
+            }
+
+            // Check if the touch is within the bounds of the exit button
+            float exitButtonX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
+            float exitButtonY = 200;
+            if (worldX >= exitButtonX && worldX <= exitButtonX + buttonWidth && worldY >= exitButtonY && worldY <= exitButtonY + buttonHeight) {
+                exitButtonClicked = true;
+            }
+
+            return true;
+        }
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        backgroundTexture.dispose();
+        playButtonTexture.dispose();
+        leaderboardButtonTexture.dispose();
+        exitButtonTexture.dispose();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
