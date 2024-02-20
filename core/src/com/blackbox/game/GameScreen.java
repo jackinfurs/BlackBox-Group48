bd89e44c0935d4ba30f5cf6788c505541de0ccb9
Jack D. <jack.dunne4@ucdconnect.ie>
2024-02-20 10:26:32 +0000

    i can't really remember the things i've done

---
+++ core/src/com/blackbox/game/GameScreen.java
@@ -0,0 +1,24 @@
+package com.blackbox.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+/*
+public class GameScreen implements Screen {
+    final BlackBox game;
+    OrthographicCamera camera;
+
+    public GameScreen(BlackBox game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        // TODO please find a way to set the viewportWidth and Height to something more concrete
+        //  this is exactly the time i'd like a preprocessor directive
+        camera.setToOrtho(false, 800, 600);
+
+    }
+}
+*/
