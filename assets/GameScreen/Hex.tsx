28c9a8161e316491ca9803cfe8a4911187149baf
Jack D. <jack.dunne4@ucdconnect.ie>
2024-02-23 15:59:17 +0000

    implemented selectTile which selects a tile with given coordinates, id[1] is black, id[2] is green contrary to what i said in previous commit

--- assets/GameScreen/Hex.tsx
+++ assets/GameScreen/Hex.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Hex" tilewidth="32" tileheight="34" tilecount="2" columns="0" objectalignment="center" tilerendersize="grid">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="0" type="Base">
   <image width="32" height="34" source="selection.png"/>
  </tile>
- <tile id="1">
+ <tile id="1" type="Select">
   <image width="32" height="34" source="friendly_target.png"/>
  </tile>
 </tileset>
--- core/src/com/blackbox/game/GameScreen.java
+++ core/src/com/blackbox/game/GameScreen.java
@@ -1,9 +1,8 @@
 package com.blackbox.game;
 
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -37,6 +36,20 @@ public class GameScreen implements Screen {
 
     }
 
+    void selectTile(TiledMap tiledMap, int x, int y) {
+//        MapProperties map = tiledMap.getProperties();
+//        if (x > map.get("width", Integer.class) - 1 ||
+//                y > map.get("height", Integer.class) - 1) {
+//            throw new IllegalArgumentException("coordinates out of bounds");
+//        } else {
+            TiledMapTile selectedTile = tiledMap.getTileSets().getTile(2); // 1 = black, 2 = green; in Tiled it's 1 for some reason
+            TiledMapTileLayer tileLayer = (TiledMapTileLayer) tiledMap.getLayers().get("Base");
+            TiledMapTileLayer.Cell cell = tileLayer.getCell(x, y);
+            cell.setTile(selectedTile);
+            renderer.render();
+        //}
+    }
+
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
@@ -50,8 +63,10 @@ public class GameScreen implements Screen {
         game.batch.begin();
 
         renderer.setView(camera);
+        camera.position.set(360, 110, 0);
         renderer.render();
-        camera.position.set(360,110,0);
+
+        selectTile(tiledMap, 4, 4);
 
         game.batch.end();
 
