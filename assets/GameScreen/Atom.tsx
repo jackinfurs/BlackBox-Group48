cc67770c2898c8c8afca3cff8cc01a0e17168891
Jack D. <jack.dunne4@ucdconnect.ie>
2024-02-23 16:59:55 +0000

    added atom tileset + assets, will write some example code next

---
+++ assets/GameScreen/Atom.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Atom" tilewidth="32" tileheight="34" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="Atom">
+  <image width="32" height="34" source="redAtom.png"/>
+ </tile>
+ <tile id="1" type="Guess">
+  <image width="32" height="34" source="guessAtom.png"/>
+ </tile>
+</tileset>
