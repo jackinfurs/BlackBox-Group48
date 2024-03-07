1838626eaa02e606b02d0a479a5e6ee061801ff0
Jack D <jack.dunne4@ucdconnect.ie>
2024-03-07 14:20:58 +0000

    might have fixed missing files

--- core/src/main/java/com/group48/blackbox/BlackBox.java
+++ core/src/main/java/com/group48/blackbox/BlackBox.java
@@ -16,8 +16,8 @@ public class BlackBox extends Game {
         font = new BitmapFont(); // use libGDX's default Arial font
         camera = new OrthographicCamera(800, 600);
         // TODO PLEASE CHANGE THIS BACK ONCE DONE
-//        this.setScreen(new MainMenuScreen(this));
-        this.setScreen(new GameScreen(this));
+        this.setScreen(new MainMenuScreen(this));
+//        this.setScreen(new GameScreen(this));
     }
     
     public void dispose()
