9c03805e8d6c62e982aa7a674bc021b78e141795
Jack D <jack.dunne4@ucdconnect.ie>
2024-03-08 20:09:10 +0000

    merged harry's approach for not placing guess atoms out of bounds, it looks nicer

--- core/src/main/java/com/group48/blackbox/Atoms.java
+++ core/src/main/java/com/group48/blackbox/Atoms.java
@@ -68,7 +68,9 @@ public class Atoms {
     
     public void addGuessAtom(int x, int y)
     {
-        if (guessAtomsCount != 6 && guessAtomsLayer.getCell(x, y) == null && !excludedCoords.contains(x + "," + y)) {
+        if (guessAtomsCount != 6 &&
+                guessAtomsLayer.getCell(x, y) == null &&
+                !isExcluded(x, y)) {
             TiledMapTileLayer.Cell guessAtomCell = new TiledMapTileLayer.Cell();
             guessAtomCell.setTile(guessTileset.getTile(2));
             guessAtomsLayer.setCell(x, y, guessAtomCell);
