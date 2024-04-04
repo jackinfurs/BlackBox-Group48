d1c975b9ef64626a89673f398bc5c9c3bd1e906d
Jack D <jack.dunne4@ucdconnect.ie>
2024-04-04 23:16:30 +0100

    redid screen resetting

--- core/src/main/java/com/group48/blackbox/GameScreen.java
+++ core/src/main/java/com/group48/blackbox/GameScreen.java
@@ -3,6 +3,7 @@ package com.group48.blackbox;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -34,6 +35,8 @@ public class GameScreen extends SignIn implements Screen {
     @Override
     public void render(float delta)
     {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
         game.camera.update();
         tiledMap.getRenderer().render();
--- core/src/main/java/com/group48/blackbox/SignInScreen.java
+++ core/src/main/java/com/group48/blackbox/SignInScreen.java
@@ -3,6 +3,7 @@ package com.group48.blackbox;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -45,6 +46,8 @@ public class SignInScreen implements Screen {
     public void render(float delta)
     {
         
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
         game.camera.update();
         game.batch.setProjectionMatrix(game.camera.combined);
